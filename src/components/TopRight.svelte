05a47bed89ce9f8d948da23a60f9ec1b5b3fa30b
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-13 17:07:34 +0530

    Create TopRight component

---
+++ src/components/TopRight.svelte
@@ -0,0 +1,9 @@
+<script>
+    import DefaultButton from "./buttons/DefaultButton.svelte";
+</script>
+
+<span class="px-4 py-2 rounded text-gray-600 hover:bg-blue-100 hover:underline">
+    <a href="#"> Log in </a>
+</span>
+
+<DefaultButton>Create account</DefaultButton>
