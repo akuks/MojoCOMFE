bfdf222b94a41984eac2730718e2645c9f7b8885
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-13 17:08:22 +0530

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    content: ["./src/**/*.{html,js,svelte}"],
+    content: [
+        "./src/**/*.{html,js,svelte}",
+        "./src/*.{html,js,svelte}",
+        "./src/**/**/*.{html,js,svelte}",
+    ],
     theme: {
         extend: {},
     },
