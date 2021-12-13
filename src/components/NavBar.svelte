6ed8231cd041305a658f32100f1b0e9ea8ba94d8
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-13 15:26:12 +0530

    Add Navbar component

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,20 @@
+<script>
+    import DefaultButton from "./buttons/DefaultButton.svelte";
+    import DefaultRound from "./buttons/DefaultRound.svelte";
+    import Profile from "./User/Profile.svelte";
+</script>
+
+<div class="w-auto ml-auto flex items-center space-x-4 h-full">
+
+    <DefaultButton>Create Post</DefaultButton>
+
+    <DefaultRound>
+        <img src="https://s.svgbox.net/hero-outline.svg?ic=chat&fill=grey-700"  class="h-6 w-6" alt=""/>
+    </DefaultRound>
+
+    <DefaultRound>
+        <img src="https://s.svgbox.net/hero-outline.svg?ic=bell&fill=grey-700" class="h-6 w-6" alt=""/>
+    </DefaultRound>
+
+<!--    <Profile />-->
+</div>
