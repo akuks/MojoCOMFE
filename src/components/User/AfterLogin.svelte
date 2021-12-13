fcbf780aaba1582ee5873461cb152378acfb483e
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-13 17:06:18 +0530

    Create AfterLogin Component

---
+++ src/components/User/AfterLogin.svelte
@@ -0,0 +1,16 @@
+<script>
+    import DefaultRound from "../buttons/DefaultRound.svelte";
+    import DefaultButton from "../buttons/DefaultButton.svelte";
+</script>
+
+<DefaultButton>Create Post</DefaultButton>
+
+<DefaultRound>
+    <img src="https://s.svgbox.net/hero-outline.svg?ic=chat&fill=grey-700"  class="h-6 w-6" alt=""/>
+</DefaultRound>
+
+<DefaultRound>
+    <img src="https://s.svgbox.net/hero-outline.svg?ic=bell&fill=grey-700" class="h-6 w-6" alt=""/>
+</DefaultRound>
+
+<!--    <Profile />-->
