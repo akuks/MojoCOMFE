6354904a8639abcc47b9737ad9e1d42417a6c24f
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-14 23:28:52 +0530

    Modify href url

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,25 @@
 <script>
 	import AppHeader from "./components/AppHeader.svelte";
 	import Article from "./components/Article.svelte";
+	import LeftColumn from "./components/LeftColumn.svelte"
 </script>
 
 <main class="min-h-screen bg-gray-100" style="margin-top: 65px">
 	<section class="container mx-auto py-8">
 		<div class="grid grid-cols-4 gap-4">
 			<AppHeader />
+			<div class="flex">
+				<LeftColumn />
+<!--				<Article />-->
+			</div>
 
-			<Article />
 		</div>
 	</section>
 </main>
+
+<style>
+	html {
+		
+	}
+</style>
 
