4d9a9b28b102117325b57ce21c8d1e6e2c88d57a
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-14 16:24:16 +0530

    Refactor Search component

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,3 +1,3 @@
 <div class="mx-4 w-96">
-    <input type="text" placeholder="Search..." class="search-bar bg-white text-gray-600 placeholder-gray-600 rounded-md p-2 h-15 bg-gray-100 w-full border border-gray-400 search-focus focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" />
+    <input type="text" placeholder="Search..." class="bg-white text-gray-600 placeholder-gray-600 rounded-md p-2 h-15 bg-white w-full border border-gray-400 search-focus focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" />
 </div>
