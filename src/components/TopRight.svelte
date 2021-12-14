ae14c81fac01d72c6ae79548c377a6786610c0b1
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-14 23:30:47 +0530

    Modify href url

--- src/components/TopRight.svelte
+++ src/components/TopRight.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <span class="px-4 py-2 rounded text-gray-600 hover:bg-blue-100 hover:underline">
-    <a href="#"> Log in </a>
+    <a href="#/"> Log in </a>
 </span>
 
 <DefaultButton>Create account</DefaultButton>
