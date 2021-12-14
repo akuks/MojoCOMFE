31b0eacfbb99f1109e89fdea2b17a47125e5f0d2
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-14 16:23:25 +0530

    Refactor AppHeader component

---
+++ src/components/AppHeader.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Logo from "./Logo.svelte";
+    import Search from "./Search.svelte";
+    import NavBar from "./NavBar.svelte";
+</script>
+
+<header class="fixed top-0 left-0 w-full bg-white border-b">
+    <div class="container mx-auto flex items-center h-16">
+        <Logo />
+        <Search />
+        <NavBar />
+    </div>
+</header>
