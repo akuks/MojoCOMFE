76bab5e727327d3898cbb5c1b3599ec572728d99
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-15 19:12:27 +0530

    RightColumn component added

--- src/components/RightColumn.svelte
+++ src/components/RightColumn.svelte
@@ -1,21 +1,10 @@
-<div class="min-h-nav">
+<div class="min-h-nav w-1/3 md:w-1/4 px-4 md:mb-0 ml-auto">
     <div class="rounded-md border overflow-hidden bg-white">
         <div class="flex items-center justify-between p-4 border-b">
             <p class="text-xl font-semibold">Listings</p>
             <p class="text-blue-600">See all</p>
         </div>
-        <listing-loader v-if="$fetchState.pending" />
-        <p v-else-if="$fetchState.error">An error occurred :(</p>
-        <ul v-else class="divide-y border-b">
-            <li v-for="(listing, l) in listings.slice(0, 5)" :key="l">
-                <div class="p-4 hover:bg-gray-50 group">
-                    <p class="text-gray-800 group-hover:text-blue-600 mb-1">
-                        {{ listing.title }}
-                    </p>
-                    <p class="text-sm text-gray-600">{{ listing.category }}</p>
-                </div>
-            </li>
-        </ul>
+
         <button class="w-full py-4 text-sm">Create a Listing</button>
     </div>
 </div>
