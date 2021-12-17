a5e3eea7cad386f11c3a9f69e9cec74b6be5361d
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-17 10:31:27 +0530

    Refactor Article component

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -1,29 +1,56 @@
+<script>
+    import ArticleTopNav from "./ArticleTopNav.svelte";
+</script>
+
 <div class="w-7/12">
-    <div class="space-y-4">
-        <div class="flex items-center justify-between">
-            <ul class="flex items-center text-gray-700">
-                <li class="px-3 py-2 rounded font-semibold hover:bg-white hover:rounded">
-                    <a class="" href="#/">Latest</a>
-                </li>
-                <li class="px-3 py-1">Week</li>
-                <li class="px-3 py-1">Month</li>
-                <li class="px-3 py-1">Year</li>
-                <li class="px-3 py-1">All</li>
-            </ul>
-        </div>
-    </div>
 
-    <div class="">
-        <div class="border rounded shadow bg-white">
+    <ArticleTopNav />
+
+    <div>
+
+        <div class="border rounded shadow bg-white mt-2">
+
             <div class="flex">
-                <div></div>
-                <div> <small> Author Name </small> </div>
-            </div>
-            <div> <small> Date </small> </div>
 
-            <div class="text-2xl"> Topic Intro </div>
+                <div class="ml-local-10 mt-2">
+                    <p class="rounded-full overflow-hidden focus:ring-2 focus:ring-offset-1 focus:ring-offset-white focus:ring-blue-800 focus:outline-none">
+                        <img src="/images/profile_default.png" class="h-8 w-8" alt="Profile" />
+                    </p>
+                </div>
+
+                <div class="ml-local-10">
+
+                    <div class="mt-2"> <small> Author Name </small> </div>
+
+                    <div> <small> Date </small> </div>
+
+                    <div class="text-2xl mt-2"> Topic Intro </div>
+
+                    <!-- Associated Tags -->
+                    <div class="flex mt-2">
+
+                        <div class="mb-6 text-gray-500 hover:bg-blue-100 hover:rounded">
+                            <div class="py-2 px-2 rounded-md border underline">
+                                <small>javascript</small>
+                            </div>
+                        </div>
+
+                        <div class="ml-2 mb-6 text-gray-500 hover:bg-blue-100 hover:rounded">
+                            <div class="py-2 px-2 rounded-md border underline">
+                                <small>beginners</small>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
 
         </div>
 
+
     </div>
+
 </div>
