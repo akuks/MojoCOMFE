085bee8c918ca99ab04daffc9ed8f0bea6969dc3
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-17 10:30:28 +0530

    TopNavigation Component

---
+++ src/components/ArticleTopNav.svelte
@@ -0,0 +1,13 @@
+<div class="space-y-4">
+    <div class="flex items-center justify-between">
+        <ul class="flex items-center text-gray-700">
+            <li class="px-3 py-2 rounded font-semibold hover:bg-white hover:rounded">
+                <a class="" href="#/">Latest</a>
+            </li>
+            <li class="px-3 py-1">Week</li>
+            <li class="px-3 py-1">Month</li>
+            <li class="px-3 py-1">Year</li>
+            <li class="px-3 py-1">All</li>
+        </ul>
+    </div>
+</div>
