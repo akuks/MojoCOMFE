c16f84ce952ff2f4179c9ed0b8979e1e9bed6d2e
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-19 23:03:41 +0530

    Integrate Routify

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,6 @@
 <script>
-	import AppHeader from "./components/AppHeader.svelte";
-	import Article from "./components/Article.svelte";
-	import LeftColumn from "./components/LeftColumn.svelte"
-	import RightColumn from "./components/RightColumn.svelte";
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
 </script>
 
-<main class="min-h-screen bg-gray-100" style="margin-top: 65px">
-	<AppHeader />
-	<section class="container mx-auto py-8">
-
-		<div class="flex flex-wrap -mx-4 -mb-4 md:mb-0">
-
-			<div class="flex w-full">
-				<LeftColumn />
-				<Article />
-				<RightColumn />
-			</div>
-
-		</div>
-	</section>
-</main>
-
-
+<Router {routes} />
