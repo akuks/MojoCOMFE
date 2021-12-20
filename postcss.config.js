e6f4aa772905986db4e36b43fb869fcadfde9a37
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-20 08:40:04 +0530

    Modify files for Routify

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
     plugins: [
-        require('tailwindcss'),
-        process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-        purgecss({
-            content: ['./src/**/*.svelte'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-    ]
-}
+        require('autoprefixer')({
+            overrideBrowserslist: ['defaults and last 4 versions'],
+        }),
+        require('postcss-import')(),
+    ],
+};
