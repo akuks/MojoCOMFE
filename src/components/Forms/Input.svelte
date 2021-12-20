abfd6566f33da767a4b4dd0479a2975a0d48f391
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-20 23:10:18 +0530

    Input Field Component

---
+++ src/components/Forms/Input.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { getContext } from 'svelte';
+
+    export let type;
+    export let label;
+    export let name;
+    export let id;
+    export let placeholder;
+
+    const { onBlur } = getContext('form');
+</script>
+
+<input class="w-full py-3 pl-3 mb-4 bg-white border rounded-lg" {id} {name} {type} {placeholder} on:blur={onBlur} />
