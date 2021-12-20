280eb96651042e3fcc89fbe48cb2bd581a0b3de5
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-20 09:08:09 +0530

    _layout.svelte and index.svelte modified

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,2 +1,28 @@
-<!-- routify:options preload="proximity" -->
-<slot />
+<script>
+    import { metatags } from '@roxi/routify'
+    import AppHeader from "../components/AppHeader.svelte";
+    import LeftColumn from "../components/LeftColumn.svelte";
+    import RightColumn from "../components/RightColumn.svelte";
+
+    metatags.title = 'MojoCom - A Community Builder in Svelte'
+    metatags.description = 'MojoCom FE Description'
+</script>
+
+<main class="min-h-screen bg-gray-100" style="margin-top: 65px">
+    <AppHeader />
+
+    <section class="container mx-auto py-8">
+
+        <div class="flex flex-wrap -mx-4 -mb-4 md:mb-0">
+
+            <div class="flex w-full">
+                <LeftColumn />
+                <slot />
+                <RightColumn />
+            </div>
+
+        </div>
+
+    </section>
+
+</main>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,26 +1,5 @@
 <script>
-    import { metatags } from '@roxi/routify'
-    import AppHeader from "../components/AppHeader.svelte";
     import Article from "../components/Article.svelte";
-    import LeftColumn from "../components/LeftColumn.svelte";
-    import RightColumn from "../components/RightColumn.svelte";
-    
-    metatags.title = 'MojoCom - A Community Builder in Svelte'
-    metatags.description = 'Description coming soon...'
 </script>
 
-<main class="min-h-screen bg-gray-100" style="margin-top: 65px">
-    <AppHeader />
-    <section class="container mx-auto py-8">
-
-        <div class="flex flex-wrap -mx-4 -mb-4 md:mb-0">
-
-            <div class="flex w-full">
-                <LeftColumn />
-                <Article />
-                <RightColumn />
-            </div>
-
-        </div>
-    </section>
-</main>
+<Article />
