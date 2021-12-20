1aead972081b7de6908953aecddaa4c0aa6fcdbc
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-20 23:09:06 +0530

    _reset.svelte for user dashboard

---
+++ src/pages/dashboard/_reset.svelte
@@ -0,0 +1,10 @@
+<script>
+    import AppHeader from "../../components/AppHeader.svelte";
+</script>
+
+<main class="min-h-screen bg-gray-100" style="margin-top: 65px">
+    <AppHeader />
+    <section class="container mx-auto">
+        <slot />
+    </section>
+</main>
